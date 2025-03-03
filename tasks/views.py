fe24abc4804096ab0f89830dd8ec751220a9d018
tobit bushenyula <115138001+tobitdacoder@users.noreply.github.com>
2025-03-03 11:12:12 +0000

    creation : views
    here I have created the views for the CRUD operations.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+# Task ViewSet for handling CRUD operations
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
